fd70b56f3d7f79044ec704589d98f8e0a830a222
Julian <juli2003juli@gmail.com>
2023-12-25 17:00:04 +0300

    primer cambio

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import SplitBillView from "./components/SplitBillView.vue";
+</script>
+
+<template>
+  <main>
+    <SplitBillView/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
